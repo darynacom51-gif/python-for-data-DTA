8fcec116e88be95c779a760b92fed3af29cf0f58
darynacom51-gif <daryna.com51@gmail.com>
2025-11-16 20:57:15 +0100

    add figure about count of genres

---
+++ main.py
@@ -0,0 +1,63 @@
+import numpy as np      # з масивами та числовими обчисленнями
+import pandas as pd     # для роботи з таблицями та даними (DataFrame)
+import matplotlib.pyplot as plt  #pyplot для графіки (частина модулю matplotlib)
+import seaborn as sns   # для стильних статистичних графіків
+
+url = "data/movies_metadata.csv"
+
+df = pd.read_csv(url) # df = DataFrame   # Завантажуємо CSV у DataFrame за допомогою pandas
+
+# print(df.head())
+# df.info()
+# print(df.describe())
+# print(df.isnull().sum())
+
+# print(df[["belongs_to_collection", "homepage", "tagline"]])
+
+# df["tagline"].fillna("without tagline", inplace=True)
+# print(df.tagline)
+
+#df.fillna({"tagline":"without tagline"}, inplace=True)
+df["tagline"] = df["tagline"].fillna("without tagline")
+
+#print(df.tagline)
+
+# print(df.homepage)
+
+df.homepage = df.homepage.fillna("no homepage")
+#print(df.homepage)
+
+#print(df["belongs_to_collection"])
+df.fillna({"belongs_to_collection":"{}"}, inplace=True)
+#print(df["belongs_to_collection"])
+
+#df.info()
+
+df.dropna(inplace=True)
+#print(df.isnull().sum())
+#df.info()
+
+
+#-------------------------------------------------------------------
+
+#print(df.head())     #виводжу таблицю
+#print(df.genres)    #жанри
+
+genres_counts = df['genres'].value_counts() # порахувати скільки жанрів
+# print(genres_counts)
+
+# print(genres_counts.index)  # X
+# print(genres_counts.values) # Y     # кількість певного жанру  (drama 4885 з 40 000 приблизно)
+
+#візуалізація
+plt.figure(figsize=(10,6)) #задали розмір
+
+sns.barplot(x=genres_counts.index, y=genres_counts.values)     #побудова графіку
+
+plt.title("Count film for genres")
+plt.xlabel("genres")
+plt.ylabel("counts")
+
+plt.xticks(rotation=45)    #як розміщені підписи
+
+plt.show()
